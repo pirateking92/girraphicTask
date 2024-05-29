f4ee772c0daade070b6d150c89a06dbaf32ea1d6
Matt Doyle <doyle9214@gmail.com>
2024-05-29 17:53:20 +0100

    trying to add more flowbite functionality to make the table look nicer. taking a bit longer than expected

--- components/AthleteList.tsx
+++ components/AthleteList.tsx
@@ -5,6 +5,14 @@
 import React from "react";
 import { Athlete } from "../types";
 import AthleteRow from "./AthleteRow";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeadCell,
+  TableRow,
+} from "flowbite-react";
 
 // set the types based on the types.ts in the root folder
 interface AthleteListProps {
@@ -14,31 +22,34 @@ interface AthleteListProps {
 
 const AthleteList: React.FC<AthleteListProps> = ({ athletes, racename }) => {
   return (
-    <div className="w-full p-6">
-      <h2 className="text-2xl font-serif text-center mb-4 bg-stone-200 rounded-md">
+    <div className="w-full p-6 overflow-auto">
+      <h2 className="text-4xl font-bold text-center mb-4 bg-stone-200 rounded-md">
         {racename}
       </h2>
-      <table className="min-w-full divide-y divide-gray-300 border border-yellow-500 rounded-md">
+      <Table
+        hoverable
+        className="min-w-full divide-y divide-gray-300 border border-yellow-500 rounded-md"
+      >
         <thead className="bg-gray-50">
           <tr>
-            <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="px-6 py-3 text-center text-s font-medium text-gray-500 uppercase tracking-wider">
               Rank
             </th>
-            <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="px-6 py-3 text-center text-s font-medium text-gray-500 uppercase tracking-wider">
               Bib Number
             </th>
-            <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="px-6 py-3 text-center text-s font-medium text-gray-500 uppercase tracking-wider">
               Name
             </th>
-            <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="px-6 py-3 text-center text-s font-medium text-gray-500 uppercase tracking-wider">
               Finish Time
             </th>
-            <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="px-6 py-3 text-center text-s font-medium text-gray-500 uppercase tracking-wider">
               Country Code
             </th>
           </tr>
         </thead>
-        <tbody className="bg-white divide-y divide-gray-200">
+        <tbody className="text-3xl tracking-wider bg-white divide-y divide-gray-200">
           {/* check that the athletes variable exists and is truthy */}
           {athletes && athletes.length > 0 ? (
             athletes.map((athlete) => (
@@ -51,7 +62,7 @@ const AthleteList: React.FC<AthleteListProps> = ({ athletes, racename }) => {
             </tr>
           )}
         </tbody>
-      </table>
+      </Table>
     </div>
   );
 };
--- components/ExportButton.tsx
+++ components/ExportButton.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { CSVLink } from "react-csv";
 import { Athlete } from "../types";
+import { Tooltip } from "flowbite-react";
 
 const generateCSV = (athletes: Athlete[] | undefined) => {
   if (!athletes || athletes.length === 0) {
@@ -33,20 +34,27 @@ const ExportButton: React.FC<ExportButtonProps> = ({ athletes }) => {
   const csvData = generateCSV(athletes);
 
   return (
-    <CSVLink
-      className="bg-gray-300 hover:bg-gray-400 border border-blue-500 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
-      data={csvData}
-      filename="race_results.csv"
+    <Tooltip
+      content="Click to download CSV file in selected order"
+      style="light"
+      animation="duration-500"
+      placement="bottom"
     >
-      <svg
-        className="fill-current w-4 h-4 mr-2"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
+      <CSVLink
+        className="bg-gray-300 hover:bg-gray-400 border border-blue-500 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+        data={csvData}
+        filename="race_results.csv"
       >
-        <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-      </svg>
-      Export to CSV
-    </CSVLink>
+        <svg
+          className="fill-current w-4 h-4 mr-2"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+        </svg>
+        Export to CSV
+      </CSVLink>
+    </Tooltip>
   );
 };
 
